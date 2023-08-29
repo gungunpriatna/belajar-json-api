d1b1104ba33c2c150cef2c690f6da5699808d45d
Gun Gun Priatna <gungun002@ummi.ac.id>
2023-08-29 11:08:44 +0700

    create seeder

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -1,6 +1,8 @@
 <?php
 
 namespace Database\Factories;
+use App\Models\User;
+use App\Models\Post;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
 
@@ -17,7 +19,11 @@ class PostFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'author_id' => User::factory(),
+            'content' => $this->faker->paragraphs(3, true),
+            'published_at' => $this->faker->boolean(75) ? $this->faker->dateTime : null,
+            'slug' => $this->faker->unique()->slug,
+            'title' => $this->faker->words(5, true),
         ];
     }
 }
